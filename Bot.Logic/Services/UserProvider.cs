6b2bbf8fa3b311b39596ab4c67d7326dc22978a8
Vladislav <vladislav.rudakoff@gmail.com>
2022-04-24 22:49:54 +0300

    Change in providers

---
+++ Bot.Logic/Services/UserProvider.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
+using Bot.Data.Enums;
+using Bot.Data.Models;
+using Bot.Logic.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bot.Logic.Services
+{
+    public class UserProvider: IUserProvider
+    {
+
+        private readonly DbContext context;
+
+        /// <summary>
+        /// ctor.
+        /// </summary>
+        /// <param name="context">Контекст БД.</param>
+        public UserProvider(DbContext context)
+        {
+            this.context = context;
+        }
+
+        public Task<IEnumerable<User>> GetAllUsers(CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<User>> GetUsersByFilter(Expression<Func<User, bool>> filter, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetOneUserByFilter(Expression<Func<User, bool>> filter, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Role> GetUserRoles(Expression<Func<User, bool>> filter, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetUserById(long userId, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
