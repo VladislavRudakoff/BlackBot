82e64b672e3d7c0daf21c714b7ddde14e935498b
Vladislav <vladislav.rudakoff@gmail.com>
2022-04-24 22:48:33 +0300

    Context and add fluent api

--- Bot.Logic/Context/BotContext.cs
+++ Bot.Logic/Context/BotContext.cs
@@ -5,13 +5,30 @@ namespace Bot.Logic.Context
 {
     public class BotContext: DbContext
     {
+        /// <summary>
+        /// Пользователи.
+        /// </summary>
         public DbSet<User> Users { get; set; } = null!;
 
-        /// <summary>
-        /// ctor.
-        /// </summary>
-        public BotContext()
+        public BotContext(DbContextOptions<BotContext> options)
+            :base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(u =>
+            {
+                u.Property(p => p.Id).UseIdentityAlwaysColumn().IsRequired();
+
+                u.HasKey(k => k.Id);
+
+                u.HasIndex(i => i.Id);
+
+                u.HasIndex(i => i.Username);
+
+                u.HasIndex(i => new { i.Id, i.Username});
+            });
         }
     }
 }
