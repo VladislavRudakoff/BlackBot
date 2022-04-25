aa43412afba2db8139846c2ffb5423704c0e0b6c
Vladislav <vladislav.rudakoff@gmail.com>
2022-04-25 21:44:14 +0300

    Added Message model

---
+++ Bot.Data/Models/Message.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Bot.Data.Models;
+
+/// <summary>
+/// Модель сообщения.
+/// </summary>
+/// <param name="UserId">Идентификатор пользователя, который отправил сообщение.</param>
+/// <param name="Text">Текст сообщения.</param>
+/// <param name="SendingTime">Время отправки сообщения.</param>
+public record Message(long UserId, int Text, DateTime SendingTime);
