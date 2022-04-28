8285697fd88d13582179d7e96f432d1abc353143
Vladislav <vladislav.rudakoff@gmail.com>
2022-04-29 00:46:33 +0300

    Added automapper

--- BotWebApi/Controllers/UserController.cs
+++ BotWebApi/Controllers/UserController.cs
@@ -1,4 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using Bot.Contracts.DTO;
+using Bot.Data.Models;
+using Bot.Logic.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Bot.WebApi.Controllers;
 
@@ -9,5 +16,20 @@ namespace Bot.WebApi.Controllers;
 [Route("api/[controller]")]
 public class UserController: Controller
 {
+    private readonly IUserProvider userProvider;
+    private readonly IMapper mapper;
 
+    public UserController(IUserProvider userProvider, IMapper mapper)
+    {
+        this.userProvider = userProvider;
+        this.mapper = mapper;
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<UserDto>> GetAllUsers(CancellationToken cancellationToken = default)
+    {
+        IEnumerable<User> users = await userProvider.GetAllUsers(cancellationToken);
+
+        return mapper.Map<IEnumerable<UserDto>>(users);
+    }
 }
