706ad120c5c1cf1e5307518d4c6fdb0ed7cbbf38
Vladislav <vladislav.rudakoff@gmail.com>
2022-05-07 19:15:34 +0300

    Added configuration extensions and added new methods yo service collection extensions

---
+++ Bot.Logic/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bot.Logic.Extensions;
+
+/// <summary>
+/// Класс с методами расширений для <see cref="IConfiguration"/>
+/// </summary>
+public static class ConfigurationExtensions
+{
+    /// <summary>
+    /// Биндит кастомные конфиги.
+    /// </summary>
+    /// <typeparam name="T">Вид конфига.</typeparam>
+    /// <param name="configuration"><see cref="IConfiguration"/>.</param>
+    /// <returns>Конфигурация.</returns>
+    public static T BindConfig<T>(this IConfiguration configuration) where T: new()
+    {
+        T instance = new T();
+
+        configuration
+            .GetSection(typeof(T).Name)
+            .Bind(new T());
+
+        return instance;
+    }
+
+    /// <summary>
+    /// Регистрирует singleton экземпляр конфига.
+    /// </summary>
+    /// <typeparam name="T">Вид конфига.</typeparam>
+    /// <param name="services"><see cref="IServiceCollection"/>.</param>
+    /// <param name="configuration"><see cref="IConfiguration"/>.</param>
+    /// <returns><see cref="IConfiguration"/>.</returns>
+    public static IServiceCollection AddConfigAsSingleton<T>(
+        this IServiceCollection services,
+        IConfiguration configuration) where T : class, new() =>
+        services.AddSingleton(_ => configuration.BindConfig<T>());
+}
