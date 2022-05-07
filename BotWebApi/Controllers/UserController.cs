e74c4f3052a56eab64ca1c958045a05db4f55cad
Vladislav <vladislav.rudakoff@gmail.com>
2022-05-07 19:18:06 +0300

    Changes to user controller

--- BotWebApi/Controllers/UserController.cs
+++ BotWebApi/Controllers/UserController.cs
@@ -1,11 +1,13 @@
-﻿using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
-using AutoMapper;
-using Bot.Contracts.DTO;
+﻿using AutoMapper;
+using Bot.Contracts.DTO.Enums;
+using Bot.Contracts.DTO.Models;
+using Bot.Data.Enums;
 using Bot.Data.Models;
 using Bot.Logic.Providers.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Bot.WebApi.Controllers;
 
@@ -14,7 +16,7 @@ namespace Bot.WebApi.Controllers;
 /// </summary>
 [ApiController]
 [Route("api/[controller]")]
-public class UserController: Controller
+public class UserController : Controller
 {
     private readonly IUserProvider userProvider;
     private readonly IMapper mapper;
@@ -49,16 +51,50 @@ public class UserController: Controller
     /// <param name="userId">Идентификатор пользователя.</param>
     /// <param name="cancellationToken">Маркер отмены.</param>
     /// <returns>Пользователь с нужным идентификатором.</returns>
-    [HttpGet("Users/{userId}")]
+    [HttpGet("{userId:int}")]
     public async Task<ActionResult<UserDto>> GetUsersById(int userId, CancellationToken cancellationToken = default)
     {
-        User? currentUser = await userProvider.GetUserById(userId, cancellationToken);
+        User? user = await userProvider.GetUserById(userId, cancellationToken);
 
-        if (currentUser is null)
+        if (user is null)
         {
             return NotFound();
         }
 
-        return mapper.Map<UserDto>(currentUser);
+        return mapper.Map<UserDto>(user);
+    }
+
+    /// <summary>
+    /// Возвращает все роли по идентификатору пользователя.
+    /// </summary>
+    /// <param name="userId">Идентификатор пользователя.</param>
+    /// <param name="cancellationToken">Маркер отмены.</param>
+    /// <returns>Роли пользователя.</returns>
+    [HttpGet("{userId}/role")]
+    public async Task<RoleDto> GetUserRoles(long userId, CancellationToken cancellationToken = default)
+    {
+        Role userRole = await userProvider.GetUserRoles(userId, cancellationToken);
+
+        return mapper.Map<RoleDto>(userRole);
+    }
+
+    /// <summary>
+    /// Возвращает пользователя по юзернейму.
+    /// </summary>
+    /// <param name="username">Юзернейм пользователя.</param>
+    /// <param name="cancellationToken">Маркер отмены.</param>
+    /// <returns>Пользователь, полученный по юзернейму.</returns>
+    [HttpGet("{username}")]
+    public async Task<ActionResult<UserDto?>> GetUserByUsername(string username,
+        CancellationToken cancellationToken = default)
+    {
+        User? user = await userProvider.GetUserByUsername(username, cancellationToken);
+
+        if (user is null)
+        {
+            return NotFound();
+        }
+
+        return mapper.Map<UserDto>(user);
     }
 }
