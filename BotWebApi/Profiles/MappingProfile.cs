c1cc495800d3938f4a28259a7f865d8fa90b3889
Vladislav <vladislav.rudakoff@gmail.com>
2022-05-07 19:14:44 +0300

    Changes to mapping profile

--- BotWebApi/Profiles/MappingProfile.cs
+++ BotWebApi/Profiles/MappingProfile.cs
@@ -1,13 +1,16 @@
 ﻿using AutoMapper;
-using Bot.Contracts.DTO;
+using Bot.Contracts.DTO.Enums;
+using Bot.Contracts.DTO.Models;
+using Bot.Data.Enums;
 using Bot.Data.Models;
 
 namespace Bot.WebApi.Profiles;
 
-public class MappingProfile: Profile
+public class MappingProfile : Profile
 {
     public MappingProfile()
     {
         CreateMap<UserDto, User>().ReverseMap();
+        CreateMap<RoleDto, Role>().ReverseMap();
     }
 }
