4541cf4a68c06f1120786ce169b6b67eef444338
Vladislav <vladislav.rudakoff@gmail.com>
2022-05-07 19:04:23 +0300

    Added policy factory

---
+++ Bot.Logic/Services/CachingPolicyFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Bot.Data.Settings;
+using Bot.Logic.Services.Interfaces;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using Polly;
+using Polly.Caching;
+using Polly.Caching.Distributed;
+using Polly.Caching.Serialization.Json;
+
+namespace Bot.Logic.Services;
+
+/// <inheritdoc cref="ICachingPolicyFactory"/>
+public class CachingPolicyFactory: ICachingPolicyFactory
+{
+    private readonly CachingSettings cachingSettings;
+    private readonly IDistributedCache distributedCache;
+
+    /// <summary>
+    /// ctor.
+    /// </summary>
+    /// <param name="cachingSettings"><see cref="CachingSettings"/>.</param>
+    /// <param name="distributedCache"><see cref="IDistributedCache"/>.</param>
+    public CachingPolicyFactory(
+        CachingSettings cachingSettings, 
+        IDistributedCache distributedCache)
+    {
+        this.cachingSettings = cachingSettings;
+        this.distributedCache = distributedCache;
+    }
+
+    public AsyncCachePolicy<T> CreateAsyncCachePolicy<T>() =>
+        Policy.CacheAsync(distributedCache.AsAsyncCacheProvider<string>()
+            .WithSerializer(new JsonSerializer<T>(new JsonSerializerSettings())), cachingSettings.CacheTtl);
+}
