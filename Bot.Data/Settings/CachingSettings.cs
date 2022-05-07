355c0d190471b1f0d5ecab582c6612609b2dc597
Vladislav <vladislav.rudakoff@gmail.com>
2022-05-07 19:06:55 +0300

    Added caching settings

---
+++ Bot.Data/Settings/CachingSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Bot.Data.Settings;
+
+/// <summary>
+/// Настройки кэширования.
+/// </summary>
+public class CachingSettings
+{
+    /// <summary>
+    /// Время жизни кэша.
+    /// </summary>
+    public TimeSpan CacheTtl { get; set; }
+}
