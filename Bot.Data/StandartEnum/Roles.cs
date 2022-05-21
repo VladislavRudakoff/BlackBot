ec2167871ca9240d4a2104077a961ef06d1232d9
Vladislav <vladislav.rudakoff@gmail.com>
2022-05-21 14:40:41 +0300

    Refactor Bot.Data

---
+++ Bot.Data/StandartEnum/Roles.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Bot.Data.StandartEnum;
+
+/// <summary>
+/// Роли.
+/// </summary>
+[Flags]
+public enum Roles
+{
+    /// <summary>
+    /// Без роли.
+    /// </summary>
+    None = 0 << 0,
+
+    /// <summary>
+    /// Пользователь.
+    /// </summary>
+    User = 1 << 0,
+
+    /// <summary>
+    /// Модератор.
+    /// </summary>
+    Moderator = 1 << 1,
+
+    /// <summary>
+    /// Админ.
+    /// </summary>
+    Admin = 1 << 2
+}
