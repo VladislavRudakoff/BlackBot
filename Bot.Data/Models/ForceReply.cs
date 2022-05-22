9664c15b3286724384f2d22213db3f023e98bf97
Vladislav <vladislav.rudakoff@gmail.com>
2022-05-22 22:39:32 +0300

    Added some models

---
+++ Bot.Data/Models/ForceReply.cs
@@ -0,0 +1,14 @@
+﻿namespace Bot.Data.Models;
+
+/// <summary>
+/// Объект для отображения пользователю интерфейса ответа.
+/// </summary>
+/// <param name="InputFieldPlaceholder">Заполнитель, который будет отображаться в поле ввода, когда ответ активен.</param>
+/// <param name="Selective">Параметр, используемый для получения ответов только от определенных пользователей: <br />
+/// - Пользователи @упомянутые в тексте сообщения <br />
+/// - Если сообщение бота является ответом на сообщение пользователя.</param>
+/// <param name="ShowForceReply">Показывает интерфейс ответа пользователю, как если бы он вручную выбрал сообщение бота и нажал «Ответить».</param>
+public record ForceReply(
+    string? InputFieldPlaceholder,
+    bool? Selective, 
+    bool ShowForceReply = true);
