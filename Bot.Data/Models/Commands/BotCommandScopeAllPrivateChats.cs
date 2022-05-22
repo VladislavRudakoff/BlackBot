cb11257e440b7bed8628408f1c62204b18fe090a
Vladislav <vladislav.rudakoff@gmail.com>
2022-05-22 22:38:40 +0300

    Added Command models

---
+++ Bot.Data/Models/Commands/BotCommandScopeAllPrivateChats.cs
@@ -0,0 +1,10 @@
+﻿namespace Bot.Data.Models.Commands;
+
+/// <summary>
+/// Область действия команд бота охватывающая все приватные чаты.
+/// </summary>
+public record BotCommandScopeAllPrivateChats()
+    : BotCommandScope
+{
+    protected override string Type { get; init; } = "all_private_chats";
+}
