d21766c091b5f65bff8f6691d39c6f63400f4674
Vladislav <vladislav.rudakoff@gmail.com>
2022-05-22 22:38:19 +0300

    Added Chat models

---
+++ Bot.Data/Models/Chats/ChatJoinRequest.cs
@@ -0,0 +1,16 @@
+﻿namespace Bot.Data.Models.Chats;
+
+/// <summary>
+/// Запрос на присоединение, отправленный в чат.
+/// </summary>
+/// <param name="Chat">Чат, в который был отправлен запрос.</param>
+/// <param name="From">Пользователь, отправивший запрос на присоединение.</param>
+/// <param name="Date">Дата отправки запроса(Unix).</param>
+/// <param name="Bio">Биография пользователя.</param>
+/// <param name="InviteLink">Ссылка, которая использовалась пользователем для отправки запроса на присоединение.</param>
+public record ChatJoinRequest(
+    Chat Chat,
+    User From,
+    int Date,
+    string? Bio,
+    ChatInviteLink? InviteLink);
