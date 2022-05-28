114004d23817bf319c8fea75b5478f393676d4db
Vladislav <vladislav.rudakoff@gmail.com>
2022-05-28 20:17:36 +0300

    Added service collection extensions

---
+++ Telegram.Contracts/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Bot.Common.Extensions;
+using Telegram.Contracts.ApiClients;
+using Telegram.Contracts.ApiClients.Interfaces;
+using Telegram.Contracts.Settings;
+
+namespace Telegram.Contracts.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection RegisterTelegramApiClient(this IServiceCollection services, IConfiguration config, TelegramClientSettings settings)
+    {
+        TelegramClientSettings clientSettings = config.BindConfig<TelegramClientSettings>();
+
+        services.AddHttpClient<ITelegramClient, TelegramClient>(
+            "TelegramBot",
+            b =>
+            {
+                b.BaseAddress = new(clientSettings.BaseUrl);
+                b.Timeout = clientSettings.TimeOut;
+            });
+
+        return services;
+    }
+
+    public static IServiceCollection RegisterSettings(this IServiceCollection services, IConfiguration config) => 
+        services.AddConfigAsSingleton<TelegramClientSettings>(config);
+}
