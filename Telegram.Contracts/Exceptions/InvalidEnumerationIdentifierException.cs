74d8a539620e79c9fd0339c72f95ee177fb5d1da
Vladislav <vladislav.rudakoff@gmail.com>
2022-07-12 22:14:32 +0300

    Refactor api clients

---
+++ Telegram.Contracts/Exceptions/InvalidEnumerationIdentifierException.cs
@@ -0,0 +1,12 @@
+﻿namespace Telegram.Contracts.Exceptions;
+
+/// <summary>
+/// Исключение для некорректного идентификатора перечисления.
+/// </summary>
+public class InvalidEnumerationIdentifierException : ApplicationException
+{
+    public InvalidEnumerationIdentifierException(int id) 
+        : base($"Invalid identifier {id}")
+    {
+    }
+}
